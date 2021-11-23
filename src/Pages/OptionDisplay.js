29c6a5d2eabe9cd53a2e9dcd921a573bdae11494
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-11-22 19:02:05 -0500

    Option Update

--- src/Pages/OptionDisplay.js
+++ src/Pages/OptionDisplay.js
@@ -3,11 +3,56 @@ import OptionCard from '../Components/OptionCard'
 
 import './OptionDisplay.css'
 
+// Functions
+function getRandomElement(items) {
+    return items[Math.floor(Math.random()*items.length)]
+}
+
 const OptionDisplay = (props) => {
 
-    console.log(props.currentRestaurants)
+    // States
+    const [options, setOptions] = useState([])
 
-    const restaurantOptionMap = props.currentRestaurants.map((value) => {
+    // Functions
+    function GenerateOptions() {
+
+        let myOptions = []
+        function populateOptions() {
+            let randomElement = getRandomElement(props.currentRestaurants)
+            if (myOptions.indexOf(randomElement) === -1) {
+                myOptions.push(randomElement)
+                // populateOptions()
+            } 
+            // else {
+            //     if (myOptions.length < 3) {
+            //         populateOptions()
+            //     } else {
+            //         console.log(myOptions.length)
+            //         return myOptions
+            //     }  
+            // }
+
+            return myOptions
+        }
+
+        populateOptions()
+
+        return myOptions
+    }
+
+    function refreshOptions() {
+        setOptions(GenerateOptions())
+    }
+
+
+    useEffect(() => {
+
+
+        setOptions(GenerateOptions())
+        
+    }, [])
+
+    const restaurantOptionMap = options.map((value) => {
 
         return(
             <OptionCard value={value} />
@@ -15,10 +60,14 @@ const OptionDisplay = (props) => {
     })
 
     return (
-        <div class="place-card__wrap--outer">
+        <div>
+            <div class="place-card__wrap--outer">
                 <div class="place-card__wrap--inner">
                     {restaurantOptionMap}
                 </div>
+            </div>
+
+            <button onClick={refreshOptions} className='submit-button'>Refresh</button>
         </div>
     )
 }
